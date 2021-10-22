883acb08dac2eecda78f20a10ae4c187472cbff3
Vladimir Unkovskiy <from_lx@mail.ru>
2021-10-22 15:14:42 +0300

    Added function 'LaterInYear'.

--- DateTime.pas
+++ DateTime.pas
@@ -1,1 +1,12 @@
-﻿function isLeapYear()
+﻿function isLeapYear(Year: integer):boolean;
+begin
+  if (Year mod 4 = 0) and (Year mod 100 <> 0) then
+    Result := True
+  else if (Year mod 400 = 0) then
+    Result := False
+end;
+
+function LaterInYear(d1, d2: DateTime): Integer;
+Begin
+  var d := DateTime.Now;
+end;
